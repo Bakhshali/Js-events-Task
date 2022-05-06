090c7dcd5d308514179af2a3b0d697cb9bcd4751
Bakhshali <tu78rfrfu@code.edu.az>
2022-05-06 12:52:18 +0400

    Task added

---
+++ assets/js/style.js
@@ -0,0 +1,9 @@
+let aze = document.querySelector('.aze')
+let tr = document.querySelector('.tr')
+let uk = document.querySelector('.uk')
+let itr = document.querySelector('.itl')
+
+aze.addEventListener('click', function(){
+    
+})
+
